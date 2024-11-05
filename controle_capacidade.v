0bb8d803104c6b9a3d09dd9d18099b1b517e606d
Julia <125603436+julia-oliver@users.noreply.github.com>
2024-11-05 16:22:12 -0300

    Add files via upload

---
+++ controle_capacidade.v
@@ -0,0 +1,13 @@
+module controle_capacidade(capacidade_atual, led_rgb);
+    input [1:0] capacidade_atual;  
+    output [2:0] led_rgb;           
+
+    wire capacidade_excedida;  
+
+    
+    and and1(capacidade_excedida, capacidade_atual[1], capacidade_atual[0]);
+
+    assign led_rgb[2] = capacidade_excedida;  
+    assign led_rgb[1] = ~capacidade_excedida; 
+    assign led_rgb[0] = 1'b0;                 
+endmodule
